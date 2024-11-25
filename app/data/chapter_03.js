53b226e786933dcdc94b3b80683650028bbc0f4c
Kirill Krymov <kkrymov.asc@gmail.com>
2024-11-25 12:18:04 +0300

    initial setup

---
+++ app/data/chapter_03.js
@@ -0,0 +1,55 @@
+export const chapter_03 = [
+    {
+      category: "Nouns",
+      words: [
+        {
+          word: "aisle",
+          synonym: "pathway",
+          antonym: "wall",
+          definition: "A passage between shelves of goods in a supermarket or other building.",
+        },
+        {
+          word: "calories",
+          synonym: "energy units",
+          antonym: "-",
+          definition: "Units of energy in food.",
+        },
+        {
+          word: "decay",
+          synonym: "rot",
+          antonym: "preserve",
+          definition: "The process of rotting or decomposing.",
+        },
+        // Add more nouns here...
+      ],
+    },
+    {
+      category: "Verbs",
+      words: [
+        {
+          word: "beat",
+          synonym: "mix hard",
+          antonym: "separate",
+          definition: "To mix something by stirring it quickly.",
+        },
+        {
+          word: "cut down on",
+          synonym: "reduce",
+          antonym: "increase",
+          definition: "To decrease the amount or number of something.",
+        },
+        // Add more verbs here...
+      ],
+    },
+    {
+      category: "Expressions",
+      words: [
+        {
+          word: "In line",
+          synonym: "waiting in a row",
+          antonym: "out of line",
+          definition: "Waiting one behind the other in a queue.",
+        },
+      ],
+    },
+  ];
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Presentation from "./components/Presentation";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+<Presentation />
+    </div>
+  );
+}
