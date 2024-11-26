3874f68c6c03f32689ebd7843d3f6e621ebfec94
Kirill Krymov <kkrymov.asc@gmail.com>
2024-11-26 15:11:15 +0300

    uninstalled reveal and installed react-awesome-reveal

--- app/components/Presentation.jsx
+++ app/components/Presentation.jsx
@@ -1,6 +1,12 @@
 "use client";
+import { Fade } from "react-awesome-reveal";
+//Database:
 import { chapter_03 } from "../data/chapter_03";
 
 export default function Presentation() {
-  <div>Hello</div>;
+  return (
+    <Fade>
+      <p>I will gently appear as I enter the viewport</p>
+    </Fade>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import "reveal.js/dist/reveal.css"; // Keep the core CSS for functionality
 import Presentation from "./components/Presentation";
 
 export default function Home() {
   return (
-    <div className="reveal">
+    <div className="w-screen h-screen">
       <Presentation />
     </div>
   );
