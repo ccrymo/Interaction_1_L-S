065b5d30ba31829812c626ba37ef5d88039d37c2
Kirill <kkrymov.asc@gmail.com>
2024-11-28 03:09:09 +0300

    ch8 bug

--- app/books/reading/chapters/ch8/page.js
+++ app/books/reading/chapters/ch8/page.js
@@ -1,11 +1,11 @@
 import WordPage from '../../../../components/Words/WordPage'
-import { chapter_04 } from "@/app/data/reading/chapter_08";
+import { chapter_08 } from "@/app/data/reading/chapter_08";
 
 
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto">
-      <WordPage chapter={chapter_04}/>
+      <WordPage chapter={chapter_08}/>
     </div>
   );
 } 
